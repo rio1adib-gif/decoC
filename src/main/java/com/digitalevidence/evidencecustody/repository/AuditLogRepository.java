1a1bc8a61e7748f097e8ece46d6aeaef8dc1a97b
rio1adib-gif <rio1adib@gmail.com>
2025-12-20 00:23:52 +0530

    Delete src directory

--- src/main/java/com/digitalevidence/evidencecustody/repository/AuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.digitalevidence.evidencecustody.repository;
-
-import com.digitalevidence.evidencecustody.entity.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-}
