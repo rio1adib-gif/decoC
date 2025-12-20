d30f1a1bd1d10222a3c8169c10730ac02a213f56
rio1adib-gif <rio1adib@gmail.com>
2025-12-20 09:16:04 +0530

    Delete src directory

--- src/assets/js/auth-guard-report.js
+++
@@ -1,7 +0,0 @@
-const role = localStorage.getItem("role");
-const token = localStorage.getItem("token");
-
-if (!token || (role !== "ROLE_ADMIN" && role !== "ROLE_OFFICER")) {
-    alert("Unauthorized access");
-    window.location.href = "/";
-}
